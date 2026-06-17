d030c3b4d4d6c8558fc5f208b7738bdeeb76c1ea
walter834 <wjave@hsperu.pe>
2026-06-17 12:23:04 -0500

    refactor(login): enhance login form structure and add loading state management

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,24 +1,26 @@
-﻿import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+﻿import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputText } from 'primeng/inputtext';
 import { Checkbox } from 'primeng/checkbox';
 import { Auth } from '../../../core/services/auth';
+import { Password } from 'primeng/password';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.html',
   styleUrl: './login.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ReactiveFormsModule, RouterLink, ButtonModule, InputText, Checkbox],
+  imports: [ReactiveFormsModule, RouterLink, ButtonModule, InputText, Checkbox,Password],
 })
 export class Login {
   private readonly fb = inject(FormBuilder);
   private readonly auth = inject(Auth);
-  private readonly router = inject(Router); 
+  private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
-
+  readonly loading = signal(false);
 
   readonly form = this.fb.nonNullable.group({
     email: ['', [Validators.required, Validators.email]],
@@ -26,16 +28,18 @@ export class Login {
   });
 
   onSubmit() {
-    if(this.form.invalid) return;
-
-    this.auth.login(this.form.getRawValue()).subscribe({
+    if (this.form.invalid) return;
+    this.loading.set(true);
+    this.auth.login(this.form.getRawValue())
+    
+    .pipe(finalize(() => this.loading.set(false)))
+    .subscribe({
       //next: () => this.router.navigate(['./sales']),
       next: () => {
         const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') ?? '/sales';
         this.router.navigateByUrl(returnUrl);
       },
       error: (err) => console.error('Login failed', err),
-    })
-
+    });
   }
 }
