be4569b4ffe55988de803689361dd59e34380a6a
walter834 <wjave@hsperu.pe>
2026-06-17 11:54:12 -0500

    refactor(auth-guard): streamline authGuard function by removing debug logs and simplifying return logic

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -2,18 +2,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { Auth } from '../services/auth';
 import { inject } from '@angular/core';
 
-  export const authGuard: CanActivateFn = (_route, state) => {
-    const auth = inject(Auth);
-    const router = inject(Router);
+export const authGuard: CanActivateFn = (_route, state) => {
+  const auth = inject(Auth);
+  const router = inject(Router);
 
-    console.log('[guard] state.url =', state.url);
-    console.log('[guard] currentUser =', auth.currentUser());
+  if (auth.currentUser()) return true;
 
-    if (auth.currentUser()) return true;
-
-    const tree = router.createUrlTree(['/login'], {
-      queryParams: { returnUrl: state.url },
-    });
-    console.log('[guard] redirijo a =', tree.toString());
-    return tree;
-  };
+  return router.createUrlTree(['/login'], {
+    queryParams: { returnUrl: state.url },
+  });
+};
