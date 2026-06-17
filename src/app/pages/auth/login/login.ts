b5f68508aff7ac80e7a361347eae21ab0d628276
walter834 <wjave@hsperu.pe>
2026-06-17 10:24:36 -0500

    feat: enhance login component with Auth service integration and form validation

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,5 +1,7 @@
 ﻿import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../../../core/services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +12,22 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class Login {
   private readonly fb = inject(FormBuilder);
+  private readonly auth = inject(Auth);
+  private readonly router = inject(Router); 
 
-  readonly form = this.fb.group({
+
+  readonly form = this.fb.nonNullable.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8)]],
   });
 
   onSubmit() {
-    console.log(this.form.value);
+    if(this.form.invalid) return;
+
+    this.auth.login(this.form.getRawValue()).subscribe({
+      next: () => this.router.navigate(['./sale']),
+      error: (err) => console.error('Login failed', err),
+    })
+
   }
 }
