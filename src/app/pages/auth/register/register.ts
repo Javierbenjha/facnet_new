02b75569f425c8f31dcd5f90ba29a6d2e0c1d1fa
walter834 <wjave@hsperu.pe>
2026-06-18 11:07:45 -0500

    refactor(register): clean up imports and improve code formatting for readability

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,39 +1,43 @@
 ﻿import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Auth } from '../../../core/services/auth';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Toaster } from '../../../core/services/toast';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.html',
   styleUrl: './register.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: []
+  imports: [],
 })
 export class Register {
   private readonly fb = inject(FormBuilder);
   private readonly auth = inject(Auth);
   private readonly router = inject(Router);
-  private readonly route = inject(ActivatedRoute);
   readonly loading = signal(false);
   private readonly toast = inject(Toaster);
 
   readonly form = this.fb.nonNullable.group({
-    nombre: ['',[Validators.required]],
-    apellido_paterno: ['',[Validators.required]],
-    apellido_materno: ['',[Validators.required]],
-    email: ['',[Validators.required, Validators.email]],
-    password: ['',[Validators.required, Validators.minLength(8)]],
-    telefono: ['',[Validators.required]]
-  })
+    nombre: ['', [Validators.required]],
+    apellido_paterno: ['', [Validators.required]],
+    apellido_materno: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    telefono: ['', [Validators.required]],
+  });
 
-  onSubmit(){
-    if(this.form.invalid) return;
+  onSubmit() {
+    if (this.form.invalid) return;
 
-    this.loading.set
+    this.loading.set(true);
+    this.auth
+      .register(this.form.getRawValue())
+      .pipe(finalize(() => this.loading.set(false)))
+      .subscribe({
+        next: () => this.router.navigate(['./login']),
+        error: (err) => this.toast.error('Error al registrarse', err.error?.message),
+      });
   }
-
-
-
 }
